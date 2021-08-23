6501b8ea4d487827cc0d9fedb43cdfcb0d77863d
vitual_puffer <547798198@qq.com>
2021-08-24 01:58:23 +0800

    邮件发送封装完成，

--- Code/netdisk_back/src/main/java/com/virtualpuffer/netdisk/service/messageService/SendMail.java
+++ Code/netdisk_back/src/main/java/com/virtualpuffer/netdisk/service/messageService/SendMail.java
@@ -1,7 +1,9 @@
 package com.virtualpuffer.netdisk.service.messageService;
 
 import com.sun.mail.util.MailSSLSocketFactory;
+import com.virtualpuffer.netdisk.DemoFactory;
 import com.virtualpuffer.netdisk.entity.User;
+import com.virtualpuffer.netdisk.service.impl.BaseServiceImpl;
 
 import javax.mail.*;
 import javax.mail.internet.AddressException;
@@ -9,31 +11,44 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.security.GeneralSecurityException;
 import java.security.PublicKey;
+import java.util.LinkedList;
 import java.util.Properties;
 
-public class SendMail extends Thread {
+public class SendMail extends BaseServiceImpl implements Runnable{
     private String From = "547798198@qq.com";
     private String recipient = "547798198@qq.com";
-    private String password = "oapzqymzhspubedj";
-    //邮件发送的服务器
+    private String password = "qykmsmflodptbeea";
+    private LinkedList<MimeMessage> list = new LinkedList<>();
     private String host = "smtp.qq.com";
+    private boolean runnable = true;
+    private static volatile SendMail sendMail;
+    public Session session;
 
-    //收件人信息
-    private User user;
-    public SendMail(User user){
-        this.user = user;
+    private SendMail(){}
+
+    public static SendMail getInstance(){
+        if(sendMail == null){//1
+            synchronized (SendMail.class){//2
+                if(sendMail == null){//3
+                    sendMail = new SendMail();//4
+                }
+            }
+        }
+        return sendMail;
+    }
+
+    public static void sendEmail(MimeMessage get){
+        getInstance().list.add(get);
     }
 
     @Override
     public void run() {
+        Transport transport = null;
         try {
             Properties properties = new Properties();
             properties.setProperty("mail.host","smtp.qq.com");
-
             properties.setProperty("mail.transport.protocol","smtp");
             properties.setProperty("mail.smtp.auth","true");
-
-            //QQ存在一个特性设置SSL加密
             MailSSLSocketFactory sf = null;
             try {
                 sf = new MailSSLSocketFactory();
@@ -43,9 +58,7 @@ public class SendMail extends Thread {
             sf.setTrustAllHosts(true);
             properties.put("mail.smtp.ssl.enable", "true");
             properties.put("mail.smtp.ssl.socketFactory", sf);
-
-            //创建一个session对象
-            Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            session = Session.getDefaultInstance(properties, new Authenticator() {
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
                     return new PasswordAuthentication(recipient,password);
@@ -53,20 +66,26 @@ public class SendMail extends Thread {
             });
             session.setDebug(false);
 
-            //获取连接对象
-            Transport transport = null;
             try {
                 transport = session.getTransport();
             } catch (NoSuchProviderException e) {
                 e.printStackTrace();
             }
-
-            System.out.println(1);
-
-            //连接服务器
             transport.connect(host,From,password);
 
-
+            while (runnable) {
+                if(!list.isEmpty()){
+                    try {
+                        MimeMessage mimeMessage = list.getFirst();
+                        transport.sendMessage(mimeMessage,mimeMessage.getAllRecipients());
+                        list.removeFirst();
+                        Thread.sleep(10000);
+                    } catch (MessagingException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException i){}
+                }
+            }
+/*
             MimeMessage mimeMessage = new MimeMessage(session);
             //邮件发送人
             mimeMessage.setFrom(new InternetAddress(recipient));
@@ -77,14 +96,11 @@ public class SendMail extends Thread {
             //邮件内容
             mimeMessage.setContent("网站注册成功，密码为"+user.getPassword()+"，请妥善保管密码","text/html;charset=UTF-8");
             //发送邮件
-            transport.sendMessage(mimeMessage,mimeMessage.getAllRecipients());
-
+*/
             transport.close();
         }catch (Exception e){
             e.printStackTrace();
         }
-
     }
-
 }
 
