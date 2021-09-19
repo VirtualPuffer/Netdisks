08af98208409de10755c2472b613537a50278a21
vitual_puffer <547798198@qq.com>
2021-09-19 18:42:25 +0800

    sql.xml完善

--- Code/netdisk_back/src/main/java/com/virtualpuffer/netdisk/mapper/blog/SpaceBlogCommentMap.java
+++ Code/netdisk_back/src/main/java/com/virtualpuffer/netdisk/mapper/blog/SpaceBlogCommentMap.java
@@ -1,4 +1,9 @@
 package com.virtualpuffer.netdisk.mapper.blog;
 
+import java.sql.Timestamp;
+
 public interface SpaceBlogCommentMap {
+    int addThumb(int comment_id,int number);
+    int deleteComment(int comment_id);
+    int makeComment(String contentText, int reply_comment_id, int blog_id, int USER_ID, Timestamp time);
 }
--- Code/netdisk_back/src/main/java/com/virtualpuffer/netdisk/mapper/blog/SpaceBlogMap.java
+++ Code/netdisk_back/src/main/java/com/virtualpuffer/netdisk/mapper/blog/SpaceBlogMap.java
@@ -6,10 +6,8 @@ import java.sql.Timestamp;
 import java.util.LinkedList;
 
 public interface SpaceBlogMap {
-    LinkedList<Blog> all();
     LinkedList<Blog> getAllBlog(int USER_ID);
     int deleteBlog(int blog_id);
     int addThumb(int blog_id);
     int makeBlog(String contentText,int USER_ID,Timestamp time);
-    int makeComment(String contentText, int reply_comment_id,int blog_id,int USER_ID,Timestamp time);
 }
